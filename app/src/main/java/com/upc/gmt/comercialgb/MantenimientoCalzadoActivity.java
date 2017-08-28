0b84d0c2a0640ce0abda29cde18883e5de16e678
Jair <jair.lorenzo.l@outlook.com>
2017-08-27 22:21:50 -0500

    Se agrego botones de Manteniemiento del calzado

--- app/src/main/java/com/upc/gmt/comercialgb/MantenimientoCalzadoActivity.java
+++ app/src/main/java/com/upc/gmt/comercialgb/MantenimientoCalzadoActivity.java
@@ -3,6 +3,7 @@ package com.upc.gmt.comercialgb;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MantenimientoCalzadoActivity extends AppCompatActivity {
 
@@ -12,4 +13,21 @@ public class MantenimientoCalzadoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_mantenimiento_calzado);
     }
 
+    public void onMantenimientoAgregarCalzado(View v) {
+        Intent i = new Intent(getApplicationContext(),com.upc.gmt.comercialgb.AgregarCalzadoActivity.class);
+
+        startActivity(i);
+    }
+
+    public void onMantenimientoEliminarCalzado(View v) {
+        Intent i = new Intent(getApplicationContext(),com.upc.gmt.comercialgb.EliminarCalzadoActivity.class);
+
+        startActivity(i);
+    }
+
+    public void onMantenimientoModificarCalzado(View v) {
+        Intent i = new Intent(getApplicationContext(), com.upc.gmt.comercialgb.EliminarCalzadoActivity.class);
+
+        startActivity(i);
+    }
 }
---
+++ app/src/main/java/com/upc/gmt/comercialgb/ModificarCalzadoActivity.java
@@ -0,0 +1,13 @@
+package com.upc.gmt.comercialgb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModificarCalzadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificar_calzado);
+    }
+}
