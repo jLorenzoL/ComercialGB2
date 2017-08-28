c8a79fcc0496adf851c07e72af584e714a3049d6
Jair <jair.lorenzo.l@outlook.com>
2017-08-27 20:02:08 -0500

    Se agrego botones de Manteniemiento del calzado

---
+++ app/src/main/java/com/upc/gmt/comercialgb/MantenimientoCalzadoActivity.java
@@ -0,0 +1,15 @@
+package com.upc.gmt.comercialgb;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MantenimientoCalzadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mantenimiento_calzado);
+    }
+
+}
--- app/src/main/java/com/upc/gmt/comercialgb/MenuPrincipalActivity.java
+++ app/src/main/java/com/upc/gmt/comercialgb/MenuPrincipalActivity.java
@@ -1,7 +1,9 @@
 package com.upc.gmt.comercialgb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MenuPrincipalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
     }
+
+    public void onIngresarMantenimientoCalzado(View v){
+        Intent i = new Intent(getApplicationContext(),com.upc.gmt.comercialgb.MantenimientoCalzadoActivity.class);
+        startActivity(i);
+    }
+
 }
