bb9f77a60ccea72079e96ca43c6e953787e021da
Jair <jair.lorenzo.l@outlook.com>
2017-09-04 16:30:02 -0500

    Se aplicó AlertDialog al activity RegistrarPedido. Se aplicó Boton Flotante

--- app/src/main/java/com/upc/gmt/comercialgb/RegistrarPedidoActivity.java
+++ app/src/main/java/com/upc/gmt/comercialgb/RegistrarPedidoActivity.java
@@ -1,7 +1,16 @@
 package com.upc.gmt.comercialgb;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static com.upc.gmt.comercialgb.R.id.btnRegistrar;
 
 public class RegistrarPedidoActivity extends AppCompatActivity {
 
@@ -9,5 +18,27 @@ public class RegistrarPedidoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_pedido);
+    }
+
+    public void onRegistrarPedidoAlert(View v){
+        AlertDialog.Builder ad= new AlertDialog.Builder(this);
+        ad.setMessage("Desea Registrar?");
+        ad.setTitle("Confirmacion");
+        ad.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(RegistrarPedidoActivity.this, "Registro Exitoso", Toast.LENGTH_LONG).show();
+            }
+        });
+        ad.setNegativeButton("Cancelar", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                cancelar();
+            }
+        });
+        ad.show();
+    }
+    public void cancelar() {
+        finish();
     }
 }
