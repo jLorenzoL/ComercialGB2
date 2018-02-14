024c23c78dd073f2f583aba42ae970df8a93ffb5
GMALEX <malexperu@gmail.com>
2018-02-13 19:00:24 -0500

    Solicitud!!!

--- app/src/main/java/com/upc/gmt/comercialgb/SolicitarAumentoActivity.java
+++ app/src/main/java/com/upc/gmt/comercialgb/SolicitarAumentoActivity.java
@@ -1,5 +1,6 @@
 package com.upc.gmt.comercialgb;
 
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -18,10 +19,14 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 public class SolicitarAumentoActivity extends AppCompatActivity {
 
+    ProgressDialog progressDialog;
+
     EditText txtCreditoTotalAumento;
     EditText txtCreditoDisponibleAumento;
     EditText txtDeudaPendienteAumento;
     EditText txtCantidadAumento;
+
+    int cantidadINT = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,8 @@ public class SolicitarAumentoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_aumento_credito);
 
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        progressDialog = new ProgressDialog(this);
 
         txtCreditoTotalAumento = (EditText) findViewById(R.id.txtCreditoTotalAumento);
         txtCreditoDisponibleAumento = (EditText) findViewById(R.id.txtCreditoDisponibleAumento);
@@ -48,10 +55,14 @@ public class SolicitarAumentoActivity extends AppCompatActivity {
         if("".equals(cantidad)){
             Toast.makeText(getApplicationContext(),"INGRESAR UNA CANTIDAD MAYOR A 0",Toast.LENGTH_LONG).show();return;
         }
-        int cantidadINT = Integer.parseInt(cantidad);
+        cantidadINT = Integer.parseInt(cantidad);
         if(cantidadINT < 1){
             Toast.makeText(getApplicationContext(),"INGRESAR UNA CANTIDAD MAYOR A 0",Toast.LENGTH_LONG).show();return;
         }
+
+        progressDialog.setTitle("Procesando...");
+        progressDialog.show();
+        new HttpRequestTaskRegistrarAumento().execute();
 
     }
 
@@ -86,6 +97,39 @@ public class SolicitarAumentoActivity extends AppCompatActivity {
             }else {
                 Util.CLIENTE_SESSION = new Cliente();
             }
+        }
+
+    }
+
+    private class HttpRequestTaskRegistrarAumento extends AsyncTask<Void, Void, Integer> {
+        @Override
+        protected Integer doInBackground(Void... params) {
+            try {
+                String URL = Util.URL_WEB_SERVICE + "/registrarSolicitud";
+                UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(URL)
+                        .queryParam("idCliente", Util.USUARIO_SESSION.getIdCliente())
+                        .queryParam("codUsuario", Util.USUARIO_SESSION.getCodUsuario())
+                        .queryParam("montoIncrementoCredito", cantidadINT);
+
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                Log.i("URL", builder.toUriString());
+                Integer i = restTemplate.getForObject(builder.build().encode().toUri(), Integer.class);
+                Log.i("Resultado", i.toString());
+                return i;
+            } catch (Exception e) {
+                Log.e(this.getClass().getName(), e.getMessage(), e);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Integer i) {
+            progressDialog.dismiss();
+            if(i == null || i == 0) {
+                Toast.makeText(getApplicationContext(), "NO SE PUDO REGISTRAR LA SOLICITUD", Toast.LENGTH_LONG).show();
+            }
+            txtCantidadAumento.setText("");
         }
 
     }
