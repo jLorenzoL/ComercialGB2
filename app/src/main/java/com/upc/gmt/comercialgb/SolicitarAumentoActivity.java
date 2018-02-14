874c41534ce6575b085e8d34edc549969eb1afa2
YanOner <rremix_2005_@hotmail.com>
2018-02-14 01:05:08 -0500

    Solicitar Aumento LISTADO

--- app/src/main/java/com/upc/gmt/comercialgb/SolicitarAumentoActivity.java
+++ app/src/main/java/com/upc/gmt/comercialgb/SolicitarAumentoActivity.java
@@ -8,14 +8,23 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.upc.gmt.model.Cliente;
+import com.upc.gmt.model.Solicitud;
 import com.upc.gmt.util.Util;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SolicitarAumentoActivity extends AppCompatActivity {
 
@@ -25,6 +34,8 @@ public class SolicitarAumentoActivity extends AppCompatActivity {
     EditText txtCreditoDisponibleAumento;
     EditText txtDeudaPendienteAumento;
     EditText txtCantidadAumento;
+
+    TextView txtDetalleAumento;
 
     int cantidadINT = 0;
 
@@ -40,8 +51,9 @@ public class SolicitarAumentoActivity extends AppCompatActivity {
         txtCreditoTotalAumento = (EditText) findViewById(R.id.txtCreditoTotalAumento);
         txtCreditoDisponibleAumento = (EditText) findViewById(R.id.txtCreditoDisponibleAumento);
         txtDeudaPendienteAumento = (EditText) findViewById(R.id.txtDeudaPendienteAumento);
-
         txtCantidadAumento = (EditText) findViewById(R.id.txtCantidadAumento);
+
+        txtDetalleAumento = (TextView) findViewById(R.id.txtDetalleAumento);
 
         new HttpRequestTaskClienteAumento().execute();
     }
@@ -51,6 +63,9 @@ public class SolicitarAumentoActivity extends AppCompatActivity {
     }
 
     public void onSolicitarAumento(View v){
+        if(!txtCantidadAumento.isEnabled()){
+            Toast.makeText(getApplicationContext(),"SOLO SE PERMITE 3 SOLICITUDES EN ESTADO PENDIENTE",Toast.LENGTH_LONG).show();return;
+        }
         String cantidad = txtCantidadAumento.getText().toString();
         if("".equals(cantidad)){
             Toast.makeText(getApplicationContext(),"INGRESAR UNA CANTIDAD MAYOR A 0",Toast.LENGTH_LONG).show();return;
@@ -94,6 +109,7 @@ public class SolicitarAumentoActivity extends AppCompatActivity {
                 txtCreditoTotalAumento.setText("S/. "+String.format("%.2f", cliente.getLineaCreditoActual()));
                 txtCreditoDisponibleAumento.setText("S/. "+String.format("%.2f", cliente.getSaldoLineaCredito()));
                 txtDeudaPendienteAumento.setText("S/. "+String.format("%.2f", cliente.getLineaCreditoActual()-cliente.getSaldoLineaCredito()));
+                new HttpRequestTaskListaAumento().execute();
             }else {
                 Util.CLIENTE_SESSION = new Cliente();
             }
@@ -130,6 +146,54 @@ public class SolicitarAumentoActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "NO SE PUDO REGISTRAR LA SOLICITUD", Toast.LENGTH_LONG).show();
             }
             txtCantidadAumento.setText("");
+            new HttpRequestTaskListaAumento().execute();
+        }
+
+    }
+
+    private class HttpRequestTaskListaAumento extends AsyncTask<Void, Void, List<Solicitud>> {
+        @Override
+        protected List<Solicitud> doInBackground(Void... params) {
+            try {
+                String URL = Util.URL_WEB_SERVICE + "/listaSolicitudes";
+                UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(URL)
+                        .queryParam("idCliente", Util.USUARIO_SESSION.getIdCliente())
+                        .queryParam("codUsuario", Util.USUARIO_SESSION.getCodUsuario());
+
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                Log.i("URL", builder.toUriString());
+                ParameterizedTypeReference<List<Solicitud>> responseType = new ParameterizedTypeReference<List<Solicitud>>() {};
+                ResponseEntity<List<Solicitud>> respuesta = restTemplate.exchange(builder.build().encode().toUri(), HttpMethod.GET, null, responseType);
+                List<Solicitud> lista = respuesta.getBody();
+                Log.i("lista", lista.toString());
+                return lista;
+            } catch (Exception e) {
+                Log.e(this.getClass().getName(), e.getMessage(), e);
+            }
+            return new ArrayList<>();
+        }
+
+        @Override
+        protected void onPostExecute(List<Solicitud> lista) {
+            String textoDetalle = "N° SOLICITUD    CANTIDAD        FECHA          ESTADO   "+ System.getProperty("line.separator");
+            int contadorPendiente = 0;
+            for (Solicitud s : lista){
+                String fechaFMT = new SimpleDateFormat("dd/MM/yyyy").format(s.getFechaSolicitud());
+                String estado = "";
+                switch (s.getIdestadosolicitud()){
+                    case 1: estado = "PENDIENTE";contadorPendiente++;break;
+                    case 2: estado = "RECHAZADO";break;
+                    default: estado = "APROBADO";
+                }
+//                textoDetalle += "" + s.getIdSolicitud() + ".       S/. " + s.getMontoIncrementoCredito().intValue() + "       " + fechaFMT + "   " + estado + System.getProperty("line.separator");
+                textoDetalle += "" + s.getIdSolicitud() + String.format("%20s", "S/. " + s.getMontoIncrementoCredito().intValue()) + String.format("%14s", fechaFMT) + String.format("%12s", estado) + System.getProperty("line.separator");
+
+            }
+            txtDetalleAumento.setText(textoDetalle);
+            if(contadorPendiente >= 3){
+                txtCantidadAumento.setEnabled(false);
+            }
         }
 
     }
